f95df3318c5b8be89da53d5f0169bb97d0f26268
iamtun <letuan19431791.iuh@gmail.com>
2024-01-14 17:22:14 +0700

    style: footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,14 @@
-﻿import React from 'react'
+﻿import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="h-[100px] flex items-center justify-between text-gray-500">
+      <div className="font-bold">tunadev</div>
+      <div className="text-xs font-medium">
+        Tuna create throughts agency &#169; All rights reserved
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
