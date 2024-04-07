bb4df4adae3fb54829afb776b97f000b1b57f953
iamtun <letuan19431791.iuh@gmail.com>
2024-04-07 15:22:38 +0700

    feat(main): use middleware

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+﻿import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log('middleware');
+  return NextResponse.next(request);
+}
+
+// See "Matching Paths" below to learn more
+// Middleware run when map with mathchers
+// Ex: above code will log when user go to posts page and posts page details
+export const config = {
+  matcher: '/posts/:path',
+};
