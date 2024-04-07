7b0478c5716ca7385bb1c3ad8122164a19a69931
iamtun <letuan19431791.iuh@gmail.com>
2024-04-07 13:23:30 +0700

    feat(main): handle catch all router

---
+++ src/app/shop/[...slug]/page.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+interface PageProps {
+  params: { slug: string[] };
+}
+
+// catch all router map with /slug1/slug2 not matching the parent router
+// catch all optional map with all routers of parent router.
+
+const Page: React.FC<PageProps> = ({ params }) => {
+  const { slug } = params;
+  return <div>{slug.join(' - ')}</div>;
+};
+
+export default Page;
